640f1daa8921bf6e12092cee41ecc6751aa271aa
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-12 17:45:07 -0400

    add adding the main files that we are going to use

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import About from './components/About';
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
