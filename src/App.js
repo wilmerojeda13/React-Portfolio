e9ea315ee9a80951fde776f7d1e786f5121730b4
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-13 08:32:13 -0400

    add add function for the nav elements that is going to render the options such as portfolio, contact,resume,

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+
+
+
+
 
 function App() {
+  const [currentNav, setNav] = useState("about")
 
-  return (
-    <div>
-      <main>
-        <About></About>
-      </main>
-    </div>
-  );
+  //function checks what user select
+   const renderNav = ()  => {
+           switch (currentNav) {
+                  case "about":
+                          return <About />;
+    
+                  case "potfolio":
+                          return <Portfolio />;
+
+                  case "contact":
+                          return <Contact />;
+
+                  case "resume":
+                          return <Resume />;
+
+                  default:
+                          return null;
+            }
+    };
+
+    return (
+          <div>
+                  <div className="mobile-header">
+                          <Header currentNav={currentNav} setNav={setNav}></Header>
+                  </div>
+                  <div>
+                         <main>{renderNav}</main>
+                  </div>
+                  <div>
+                         <Footer></Footer>
+                  </div>
+            </div>
+    )
 }
 
 export default App;
+
